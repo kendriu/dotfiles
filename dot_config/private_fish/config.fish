09d7c3d4013c658d828ce3bc0dd8b2e27cd5a380
Andrzej Skupien <kendriu@gmail.com>
2024-12-17 22:09:45 +0100

    More changes

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,7 +1,14 @@
 export LANG=us_US.utf-8
+export EDITOR=nvim
+
+
 if not functions -q fundle; eval (curl -sfL https://git.io/fundle-install); end
 fundle plugin 'jethrokuan/z'
+fundle plugin 'jorgebucaran/autopair.fish'
+fundle plugin 'franciscolourenco/done'
 fundle init
+
+fish_add_path /opt/homebrew/opt/rustup/bin
 
 eval "$(/opt/homebrew/bin/brew shellenv)"
 
@@ -17,9 +24,19 @@ abbr rz fish
 abbr c chezmoi
 abbr ca chezmoi apply -v
 
+# brew
+function cb -d "Change brew file & install"
+    set fish_trace 1
+    nvim ~/.local/share/chezmoi/run_onchange_install-packages.sh.tmpl 
+    chezmoi apply -v
+
+end
+
 function c-sync
     cd ~/.local/share/chezmoi/
     git add .
     git commit -m"$argv"
     git push
 end
+
+test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
