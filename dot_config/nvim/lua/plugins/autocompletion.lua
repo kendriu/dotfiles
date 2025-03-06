eca2beffd9e6e9c035686e1a1de5683ed8951d16
Andrzej Skupień <kendriu@gmail.com>
2025-03-06 17:32:07 +0100

    Sync

--- dot_config/nvim/lua/plugins/autocompletion.lua
+++ dot_config/nvim/lua/plugins/autocompletion.lua
@@ -1,109 +1,103 @@
-return { -- Autocompletion
-	"hrsh7th/nvim-cmp",
-	event = "InsertEnter",
+return {
+	"stevearc/conform.nvim",
 	dependencies = {
-		-- Snippet Engine & its associated nvim-cmp source
-		{
-			"L3MON4D3/LuaSnip",
-			build = (function()
-				-- Build Step is needed for regex support in snippets.
-				-- This step is not supported in many windows environments.
-				-- Remove the below condition to re-enable on windows.
-				if vim.fn.has("win32") == 1 or vim.fn.executable("make") == 0 then
+		"nvimtools/none-ls.nvim",
+		"jayp0521/mason-null-ls.nvim", -- ensure dependencies are installed
+	},
+	event = { "BufReadPre", "BufNewFile" },
+	config = function()
+		require("mason-null-ls").setup({
+			ensure_installed = {
+				"just",
+				"shfmt",
+				"stylua", -- lua formatter
+				"ruff",
+				"rustfmt",
+				"prettier",
+			},
+			automatic_installation = true,
+		})
+
+		local conform = require("conform")
+		local default_format_options = {
+			lsp_fallback = true,
+			async = false,
+			timeout = 500,
+		}
+		local format_hunks = function()
+			local ignore_filetypes = { "lua" }
+			if vim.tbl_contains(ignore_filetypes, vim.bo.filetype) then
+				vim.notify("range formatting for " .. vim.bo.filetype .. " not working properly.")
+				return default_format_options
+			end
+
+			local hunks = require("gitsigns").get_hunks()
+			if hunks == nil then
+				return default_format_options
+			end
+
+			local format = require("conform").format
+
+			local function format_range()
+				if next(hunks) == nil then
+					vim.notify("done formatting git hunks", "info", { title = "formatting" })
 					return
 				end
-				return "make install_jsregexp"
-			end)(),
-			dependencies = {
-				-- `friendly-snippets` contains a variety of premade snippets.
-				--    See the README about individual language/framework/plugin snippets: '
-				--    https://github.com/rafamadriz/friendly-snippets
-				{
-					"rafamadriz/friendly-snippets",
-					config = function()
-						require("luasnip.loaders.from_vscode").lazy_load()
-					end,
-				},
+				local hunk = nil
+				while next(hunks) ~= nil and (hunk == nil or hunk.type == "delete") do
+					hunk = table.remove(hunks)
+				end
+
+				if hunk ~= nil and hunk.type ~= "delete" then
+					local start = hunk.added.start
+					local last = start + hunk.added.count
+					-- nvim_buf_get_lines uses zero-based indexing -> subtract from last
+					local last_hunk_line = vim.api.nvim_buf_get_lines(0, last - 2, last - 1, true)[1]
+					local range = { start = { start, 0 }, ["end"] = { last - 1, last_hunk_line:len() } }
+					format({ range = range, async = true, lsp_fallback = true }, function()
+						vim.defer_fn(function()
+							format_range()
+						end, 1)
+					end)
+				end
+			end
+
+			format_range()
+		end
+
+		conform.setup({
+			formatters_by_ft = {
+				json = { "prettier" },
+				just = { "just" },
+				lua = { "stylua" },
+				markdown = { "prettier" },
+				python = { "ruff_format" },
+				sh = { "shfmt" },
+				yaml = { "prettier" },
 			},
-		},
-		"saadparwaiz1/cmp_luasnip",
-
-		-- Adds other completion capabilities.
-		--  nvim-cmp does not ship with all sources by default. They are split
-		--  into multiple repos for maintenance purposes.
-		"hrsh7th/cmp-nvim-lsp",
-		"hrsh7th/cmp-path",
-	},
-	config = function()
-		-- See `:help cmp`
-		local cmp = require("cmp")
-		local luasnip = require("luasnip")
-		luasnip.config.setup({})
-
-		cmp.setup({
-			snippet = {
-				expand = function(args)
-					luasnip.lsp_expand(args.body)
-				end,
-			},
-			completion = { completeopt = "menu,menuone,noinsert" },
-
-			-- For an understanding of why these mappings were
-			-- chosen, you will need to read `:help ins-completion`
-			--
-			-- No, but seriously. Please read `:help ins-completion`, it is really good!
-			mapping = cmp.mapping.preset.insert({
-				-- Select the [n]ext item
-				["<Tab>"] = cmp.mapping.select_next_item(),
-				-- Select the [p]revious item
-				["<S-Tab>"] = cmp.mapping.select_prev_item(),
-
-				-- Scroll the documentation window [b]ack / [f]orward
-				-- ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-				-- ["<C-f>"] = cmp.mapping.scroll_docs(4),
-
-				-- Accept ([y]es) the completion.
-				--  This will auto-import if your LSP supports it.
-				--  This will expand snippets if the LSP sent a snippet.
-				["<CR>"] = cmp.mapping.confirm({ select = true }),
-
-				-- Manually trigger a completion from nvim-cmp.
-				--  Generally you don't need this, because nvim-cmp will display
-				--  completions whenever it has completion options available.
-				["<C-Space>"] = cmp.mapping.complete({}),
-
-				-- Think of <c-l> as moving to the right of your snippet expansion.
-				--  So if you have a snippet that's like:
-				--  function $name($args)
-				--    $body
-				--  end
-				--
-				-- <c-l> will move you to the right of each of the expansion locations.
-				-- <c-h> is similar, except moving you backwards.
-				["<C-l>"] = cmp.mapping(function()
-					if luasnip.expand_or_locally_jumpable() then
-						luasnip.expand_or_jump()
-					end
-				end, { "i", "s" }),
-				["<C-h>"] = cmp.mapping(function()
-					if luasnip.locally_jumpable(-1) then
-						luasnip.jump(-1)
-					end
-				end, { "i", "s" }),
-
-				-- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
-				--    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
-			}),
-			sources = {
-				{
-					name = "lazydev",
-					-- set group index to 0 to skip loading LuaLS completions as lazydev recommends it
-					group_index = 0,
-				},
-				{ name = "nvim_lsp" },
-				{ name = "luasnip" },
-				{ name = "path" },
-			},
+			format_on_save = function()
+				return format_hunks()
+			end,
 		})
+		conform.formatters.ruff_format = {
+			args = { "format", "--force-exclude", "--line-length", "140", "--stdin-filename", "$FILENAME", "-" },
+			range_args = function(self, ctx)
+				return {
+					"format",
+					"--force-exclude",
+					"--range",
+					string.format(
+						"%d:%d-%d:%d",
+						ctx.range.start[1],
+						ctx.range.start[2] + 1,
+						ctx.range["end"][1],
+						ctx.range["end"][2] + 1
+					),
+					"--stdin-filename",
+					"$FILENAME",
+					"-",
+				}
+			end,
+		}
 	end,
 }
