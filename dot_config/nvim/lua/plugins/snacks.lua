662b17b34c42cd842a920502b538caff3a3c1a06
Andrzej Skupien <kendriu@gmail.com>
2025-03-07 08:53:50 +0100

    Sync

--- dot_config/nvim/lua/plugins/snacks.lua
+++ dot_config/nvim/lua/plugins/snacks.lua
@@ -8,7 +8,15 @@ return {
 		{ "<leader>bo", function() Snacks.bufdelete.other() end, desc = "Delete other buffers", },
 		-- picker
 		{ "<leader>ss", function() Snacks.picker.lsp_symbols() end, desc = "LSP Symbols" },
-		{ "<leader>sS", function() Snacks.picker.lsp_workspace_symbols() end, desc = "LSP Workspace Symbols" },
+		{ "<leader>sS", function() Snacks.picker.lsp_workspace_symbols() end, desc = "LSP Project Symbols" },
+		{ "<leader>/", function() Snacks.picker.lines({ layout={ preset="ivy_split" } }) end, desc = "Buffer Lines"},
+		-- picker find
+		{ "<leader>fb", function() Snacks.picker.buffers() end, desc = "Buffers" },
+		{ "<leader>fc", function() Snacks.picker.files({ cwd = vim.fn.stdpath("config") }) end, desc = "Find Config File" },
+		{ "<leader>ff", function() Snacks.picker.files() end, desc = "Find Files" },
+		{ "<leader>fg", function() Snacks.picker.git_files() end, desc = "Find Git Files" },
+		{ "<leader>fp", function() Snacks.picker.projects() end, desc = "Projects" },
+		{ "<leader>fr", function() Snacks.picker.recent() end, desc = "Recent" },
 		-- stylua: ignore stop
 	},
 	---@type snacks.Config
@@ -16,4 +24,5 @@ return {
 		bufdelete = { enabled = true },
 		picker = { enabled = true },
 	},
+	vim.api.nvim_set_hl(0, "SnacksPickerDir", { link = "SpecialComment" }),
 }
--- dot_config/nvim/lua/plugins/telescope.lua
+++ dot_config/nvim/lua/plugins/telescope.lua
@@ -83,15 +83,6 @@ return { -- Fuzzy Finder (files, lsp, etc)
 			{ desc = "[S]earch [T]odo" }
 		)
 
-		-- Slightly advanced example of overriding default behavior and theme
-		vim.keymap.set("n", "<leader>/", function()
-			-- You can pass additional configuration to Telescope to change the theme, layout, etc.
-			builtin.current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
-				winblend = 10,
-				previewer = false,
-			}))
-		end, { desc = "[/] Fuzzily search in current buffer" })
-
 		-- It's also possible to pass additional configuration options.
 		--  See `:help telescope.builtin.live_grep()` for information about particular keys
 		vim.keymap.set("n", "<leader>s/", function()
