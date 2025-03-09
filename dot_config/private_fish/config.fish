859e8d880b627b5aa6cb1ab23e05330dd3d10c99
Andrzej Skupien <kendriu@gmail.com>
2025-03-09 23:43:10 +0100

    Update .config/fish/config.fish Update install-packages.sh

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -46,6 +46,16 @@ function ls --wraps eza
     eza --icons=auto --group-directories-first $argv
 end
 
+# yazi
+function y
+    set tmp (mktemp -t "yazi-cwd.XXXXXX")
+    yazi $argv --cwd-file="$tmp"
+    if set cwd (command cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+        builtin cd -- "$cwd"
+    end
+    rm -f -- "$tmp"
+end
+
 # chezmoi
 abbr ch chezmoi
 abbr chc cd ~/.local/share/chezmoi/
