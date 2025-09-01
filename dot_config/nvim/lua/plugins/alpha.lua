d9b07ff107263b7dbd0554117dce563e1d49f3fd
Andrzej Skupień <kendriu@gmail.com>
2025-09-01 13:51:44 +0200

    Add .config/nvim/init.lua Add .config/nvim/lazy-lock.json Add .config/nvim/lua/core/keymaps.lua Add .config/nvim/lua/core/neovide.lua Add .config/nvim/lua/core/options.lua Add .config/nvim/lua/plugins/alpha.lua Add .config/nvim/lua/plugins/autoformatting.lua Add .config/nvim/lua/plugins/blink.lua Add .config/nvim/lua/plugins/bufferline.lua Add .config/nvim/lua/plugins/chezmoi.lua Add .config/nvim/lua/plugins/colortheme.lua Add .config/nvim/lua/plugins/conform.lua Add .config/nvim/lua/plugins/delimiters.lua Add .config/nvim/lua/plugins/diagnostics.lua Add .config/nvim/lua/plugins/docs.lua Add .config/nvim/lua/plugins/.luarc.json Add .config/nvim/lua/plugins/flash.lua Add .config/nvim/lua/plugins/gitsigns.lua Add .config/nvim/lua/plugins/http.lua Add .config/nvim/lua/plugins/ibl.lua Add .config/nvim/lua/plugins/indent-object.lua Add .config/nvim/lua/plugins/lsp.lua Add .config/nvim/lua/plugins/lualine.lua Add .config/nvim/lua/plugins/misc.lua Add .config/nvim/lua/plugins/navi-note.lua Add .config/nvim/lua/plugins/session.lua Add .config/nvim/lua/plugins/snacks.lua Add .config/nvim/lua/plugins/todo.lua Add .config/nvim/lua/plugins/treesitter.lua Add .config/nvim/lua/plugins/trouble.lua

---
+++ dot_config/nvim/lua/plugins/alpha.lua
@@ -0,0 +1,68 @@
+return {
+	"goolord/alpha-nvim",
+	-- dependencies = { 'echasnovski/mini.icons' },
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	config = function()
+		local startify = require("alpha.themes.startify")
+
+		-- Define a function to load the last session
+		--- @param sc string
+		--- @param txt string
+		--- @param keybind string? optional
+		--- @param keybind_opts table? optional
+		--- @param hl_opts table? optional
+		local function button(sc, txt, keybind, keybind_opts, hl_opts)
+			local leader = "SPC"
+			local sc_ = sc:gsub("%s", ""):gsub(leader, "<leader>")
+
+			local opts = {
+				position = "left",
+				shortcut = "[" .. sc .. "] ",
+				cursor = 1,
+				-- width = 50,
+				align_shortcut = "left",
+				hl_shortcut = { { "Operator", 0, 1 }, { "Number", 1, #sc + 1 }, { "Operator", #sc + 1, #sc + 2 } },
+				shrink_margin = false,
+				hl = { hl_opts },
+			}
+			if keybind then
+				keybind_opts = vim.F.if_nil(keybind_opts, { noremap = true, silent = true, nowait = true })
+				opts.keymap = { "n", sc_, keybind, keybind_opts }
+			end
+
+			local function on_press()
+				local key = vim.api.nvim_replace_termcodes(keybind .. "<Ignore>", true, false, true)
+				vim.api.nvim_feedkeys(key, "t", false)
+			end
+
+			return {
+				type = "button",
+				val = txt,
+				on_press = on_press,
+				opts = opts,
+			}
+		end
+
+		local utils = require("session_manager.utils")
+		local dir = utils.get_sessions()[1].dir.filename
+
+		local lls_txt = "Load Last Session "
+		local session_group = {
+			type = "group",
+			val = {
+				button(
+					"l",
+					lls_txt .. dir,
+					"<cmd> SessionManager load_last_session <cr>",
+					{},
+					{ "Comment", #lls_txt, #lls_txt + #dir }
+				),
+				button("L", "Load Session", "<cmd> SessionManager load_session <cr>"),
+			},
+		}
+		local startify_config = startify.config
+		table.insert(startify_config.layout, #startify_config.layout - 1, session_group)
+		table.insert(startify_config.layout, #startify_config.layout - 1, { type = "padding", val = 1 })
+		require("alpha").setup(startify.config)
+	end,
+}
