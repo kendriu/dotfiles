f46c8578386da442a801b909dd77ea90197e7b06
Andrzej Skupień <kendriu@gmail.com>
2025-09-03 17:45:58 +0200

    Remove .config/nvim/init.lua Remove .config/nvim/lazy-lock.json Remove .config/nvim/lua/core/keymaps.lua Remove .config/nvim/lua/core/neovide.lua Remove .config/nvim/lua/core/options.lua Remove .config/nvim/lua/plugins/alpha.lua Remove .config/nvim/lua/plugins/autoformatting.lua Remove .config/nvim/lua/plugins/blink.lua Remove .config/nvim/lua/plugins/bufferline.lua Remove .config/nvim/lua/plugins/chezmoi.lua Remove .config/nvim/lua/plugins/colortheme.lua Remove .config/nvim/lua/plugins/conform.lua Remove .config/nvim/lua/plugins/delimiters.lua Remove .config/nvim/lua/plugins/diagnostics.lua Remove .config/nvim/lua/plugins/docs.lua Remove .config/nvim/lua/plugins/.luarc.json Remove .config/nvim/lua/plugins/flash.lua Remove .config/nvim/lua/plugins/gitsigns.lua Remove .config/nvim/lua/plugins/http.lua Remove .config/nvim/lua/plugins/ibl.lua Remove .config/nvim/lua/plugins/indent-object.lua Remove .config/nvim/lua/plugins/lsp.lua Remove .config/nvim/lua/plugins/lualine.lua Remove .config/nvim/lua/plugins/misc.lua Remove .config/nvim/lua/plugins/navi-note.lua Remove .config/nvim/lua/plugins/session.lua Remove .config/nvim/lua/plugins/snacks.lua Remove .config/nvim/lua/plugins/todo.lua Remove .config/nvim/lua/plugins/treesitter.lua Remove .config/nvim/lua/plugins/trouble.lua

--- dot_config/nvim/lua/plugins/docs.lua
+++
@@ -1,9 +0,0 @@
-return {
-	"amrbashir/nvim-docs-view",
-	cmd = "DocsViewToggle",
-	opts = {
-		position = "right",
-		width = 70,
-	},
-	vim.keymap.set("n", "<leader>td", ":DocsViewToggle<CR>", { desc = "[T]oggle [D]ocs" }),
-}
