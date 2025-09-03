a5e46c045828e9fc29a780fc47d4222567f201d2
Andrzej Skupień <kendriu@gmail.com>
2025-09-03 17:33:43 +0200

    Add .config/nvim/init.lua Add .config/nvim/lazy-lock.json Add .config/nvim/lua/core/keymaps.lua Add .config/nvim/lua/core/neovide.lua Add .config/nvim/lua/core/options.lua Add .config/nvim/lua/plugins/alpha.lua Add .config/nvim/lua/plugins/autoformatting.lua Add .config/nvim/lua/plugins/blink.lua Add .config/nvim/lua/plugins/bufferline.lua Add .config/nvim/lua/plugins/chezmoi.lua Add .config/nvim/lua/plugins/colortheme.lua Add .config/nvim/lua/plugins/conform.lua Add .config/nvim/lua/plugins/delimiters.lua Add .config/nvim/lua/plugins/diagnostics.lua Add .config/nvim/lua/plugins/docs.lua Add .config/nvim/lua/plugins/.luarc.json Add .config/nvim/lua/plugins/flash.lua Add .config/nvim/lua/plugins/gitsigns.lua Add .config/nvim/lua/plugins/http.lua Add .config/nvim/lua/plugins/ibl.lua Add .config/nvim/lua/plugins/indent-object.lua Add .config/nvim/lua/plugins/lsp.lua Add .config/nvim/lua/plugins/lualine.lua Add .config/nvim/lua/plugins/misc.lua Add .config/nvim/lua/plugins/navi-note.lua Add .config/nvim/lua/plugins/session.lua Add .config/nvim/lua/plugins/snacks.lua Add .config/nvim/lua/plugins/todo.lua Add .config/nvim/lua/plugins/treesitter.lua Add .config/nvim/lua/plugins/trouble.lua

---
+++ dot_config/nvim/lua/plugins/http.lua
@@ -0,0 +1,16 @@
+return {
+	{
+		"mistweaverco/kulala.nvim",
+		-- keys = {
+		-- 	{ "<leader>as", desc = "Send request" },
+		-- 	{ "<leader>aa", desc = "Send all requests" },
+		-- 	{ "<leader>ab", desc = "Open scratchpad" },
+		-- },
+		ft = { "http", "rest" },
+		opts = {
+			global_keymaps = true,
+			global_keymaps_prefix = "<leader>k",
+			kulala_keymaps_prefix = "",
+		},
+	},
+}
