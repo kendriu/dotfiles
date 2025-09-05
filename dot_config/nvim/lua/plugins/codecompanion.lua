cfe2972db46c1dcc8a5eb2eaae0bc48dc1e03e71
Andrzej Skupień <kendriu@gmail.com>
2025-09-05 12:13:26 +0200

    Add .config/nvim/init.lua Add .config/nvim/lazy-lock.json Add .config/nvim/lua/core/keymaps.lua Add .config/nvim/lua/core/neovide.lua Add .config/nvim/lua/core/options.lua Add .config/nvim/lua/plugins/alpha.lua Add .config/nvim/lua/plugins/autoformatting.lua Add .config/nvim/lua/plugins/blink.lua Add .config/nvim/lua/plugins/bufferline.lua Add .config/nvim/lua/plugins/chezmoi.lua Add .config/nvim/lua/plugins/codecompanion.lua Add .config/nvim/lua/plugins/colortheme.lua Add .config/nvim/lua/plugins/conform.lua Add .config/nvim/lua/plugins/delimiters.lua Add .config/nvim/lua/plugins/diagnostics.lua Add .config/nvim/lua/plugins/docs.lua Add .config/nvim/lua/plugins/.luarc.json Add .config/nvim/lua/plugins/flash.lua Add .config/nvim/lua/plugins/gitsigns.lua Add .config/nvim/lua/plugins/http.lua Add .config/nvim/lua/plugins/ibl.lua Add .config/nvim/lua/plugins/indent-object.lua Add .config/nvim/lua/plugins/lsp.lua Add .config/nvim/lua/plugins/lualine.lua Add .config/nvim/lua/plugins/misc.lua Add .config/nvim/lua/plugins/navi-note.lua Add .config/nvim/lua/plugins/session.lua Add .config/nvim/lua/plugins/snacks.lua Add .config/nvim/lua/plugins/todo.lua Add .config/nvim/lua/plugins/treesitter.lua Add .config/nvim/lua/plugins/trouble.lua

---
+++ dot_config/nvim/lua/plugins/codecompanion.lua
@@ -0,0 +1,63 @@
+return {
+	"olimorris/codecompanion.nvim",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"folke/snacks.nvim", -- use Snacks instead of Telescope
+	},
+	config = function()
+		local cc = require("codecompanion")
+
+		cc.setup({
+			strategies = {
+				chat = { adapter = "qwen" },
+				inline = { adapter = "qwen" },
+			},
+			adapters = {
+				http = {
+					qwen = function()
+						return require("codecompanion.adapters").extend("ollama", {
+							name = "CodeLlama",
+							endpoint = "http://localhost:11434",
+							model = "qwen2.5:7b-instruct",
+						})
+					end,
+					codellama = function()
+						return require("codecompanion.adapters").extend("ollama", {
+							name = "CodeLlama",
+							endpoint = "http://localhost:11434",
+							model = "codellama:7b-instruct",
+						})
+					end,
+					deepseek = function()
+						return require("codecompanion.adapters").extend("ollama", {
+							name = "DeepSeek",
+							endpoint = "http://localhost:11434",
+							model = "deepseek-coder:6.7b",
+						})
+					end,
+				},
+			},
+
+			-- 👇 Replace telescope with snacks as the picker
+			display = {
+				chat = {
+					provider = "snacks",
+				},
+				inline = {
+					provider = "snacks",
+				},
+			},
+		})
+
+		-- Optional: keymaps for quick switching
+		vim.keymap.set("n", "<leader>ml", function()
+			cc.config.strategies.chat.adapter = "codellama"
+			print("🔄 Switched to Code Llama for chat")
+		end, { desc = "Use Code Llama" })
+
+		vim.keymap.set("n", "<leader>md", function()
+			cc.config.strategies.chat.adapter = "deepseek"
+			print("🔄 Switched to DeepSeek for chat")
+		end, { desc = "Use DeepSeek" })
+	end,
+}
