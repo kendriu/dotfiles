47cde97b2bc409bb85c0802668ae61fbcfdb02ef
Andrzej Skupień <kendriu@gmail.com>
2026-01-23 12:16:35 +0100

    Add .config/nvim/init.lua Add .config/nvim/lazy-lock.json Add .config/nvim/lua/core/keymaps.lua Add .config/nvim/lua/core/neovide.lua Add .config/nvim/lua/core/options.lua Add .config/nvim/lua/plugins/alpha.lua Add .config/nvim/lua/plugins/autoformatting.lua Add .config/nvim/lua/plugins/blink.lua Add .config/nvim/lua/plugins/bufferline.lua Add .config/nvim/lua/plugins/chezmoi.lua Add .config/nvim/lua/plugins/codecompanion.lua Add .config/nvim/lua/plugins/colortheme.lua Add .config/nvim/lua/plugins/conform.lua Add .config/nvim/lua/plugins/copilot.lua Add .config/nvim/lua/plugins/delimiters.lua Add .config/nvim/lua/plugins/diagnostics.lua Add .config/nvim/lua/plugins/docs.lua Add .config/nvim/lua/plugins/.luarc.json Add .config/nvim/lua/plugins/flash.lua Add .config/nvim/lua/plugins/gitsigns.lua Add .config/nvim/lua/plugins/http.lua Add .config/nvim/lua/plugins/ibl.lua Add .config/nvim/lua/plugins/indent-object.lua Add .config/nvim/lua/plugins/lsp.lua Add .config/nvim/lua/plugins/lualine.lua Add .config/nvim/lua/plugins/misc.lua Add .config/nvim/lua/plugins/render-markdown.lua Add .config/nvim/lua/plugins/sidekick.lua Add .config/nvim/lua/plugins/snacks.lua Add .config/nvim/lua/plugins/spelunk.lua Add .config/nvim/lua/plugins/todo.lua Add .config/nvim/lua/plugins/treesitter.lua Add .config/nvim/lua/plugins/trouble.lua

---
+++ dot_config/nvim/lua/plugins/blink.lua
@@ -0,0 +1,145 @@
+return {
+	"saghen/blink.cmp",
+	event = "VeryLazy",
+	-- optional: provides snippets for the snippet source
+	dependencies = "rafamadriz/friendly-snippets",
+	-- NOTE: if blink doesn't work uncomment the line bellow and run Lazy update
+	version = "*",
+	-- build = "cargo build --release",
+
+	-- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
+	-- build = 'cargo build --release',
+	-- If you use nix, you can build from source using latest nightly rust with:
+	-- build = 'nix run .#build-plugin',
+
+	---@module 'blink.cmp'
+	---@type blink.cmp.Config
+	opts = {
+		-- 'default' (recommended) for mappings similar to built-in completions (C-y to accept, C-n/C-p for up/down)
+		-- 'super-tab' for mappings similar to vscode (tab to accept, arrow keys for up/down)
+		-- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
+		--
+		-- All presets have the following mappings:
+		-- C-space: Open menu or open docs if already open
+		-- C-e: Hide menu
+		-- C-k: Toggle signature help
+		--
+		-- See the full "keymap" documentation for information on defining your own keymap.
+		keymap = {
+			preset = "none",
+			["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
+			["<C-e>"] = { "hide", "fallback" },
+			-- ["<CR>"] = { "accept", "fallback" },
+			["<Tab>"] = {
+				function(cmp)
+					if cmp.snippet_active() then
+						return cmp.accept()
+					else
+						return cmp.select_and_accept()
+					end
+				end,
+				function() -- sidekick next edit suggestion
+					return require("sidekick").nes_jump_or_apply()
+				end,
+
+				function() -- if you are using Neovim's native inline completions
+					return vim.lsp.inline_completion.get()
+				end,
+				"snippet_forward",
+				"fallback",
+			},
+
+			["<S-Tab>"] = { "snippet_backward", "fallback" },
+
+			["<C-j>"] = { "select_next", "fallback" },
+			["<C-k>"] = { "select_prev", "fallback" },
+			["<C-n>"] = { "select_next", "fallback_to_mappings" },
+			["<C-p>"] = { "select_prev", "fallback_to_mappings" },
+
+			["<C-f>"] = { "scroll_documentation_down", "fallback" },
+			["<C-b>"] = { "scroll_documentation_up", "fallback" },
+
+			["<C-y>"] = { "show_signature", "hide_signature", "fallback" },
+		},
+
+		appearance = {
+			-- Sets the fallback highlight groups to nvim-cmp's highlight groups
+			-- Useful for when your theme doesn't support blink.cmp
+			-- Will be removed in a future release
+			use_nvim_cmp_as_default = true,
+			-- Set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+			-- Adjusts spacing to ensure icons are aligned
+			nerd_font_variant = "mono",
+		},
+		signature = {
+			enabled = true,
+		},
+		completion = {
+			ghost_text = {
+				enabled = true,
+				-- Show the ghost text when an item has been selected
+				show_with_selection = true,
+				-- Show the ghost text when no item has been selected, defaulting to the first item
+				show_without_selection = false,
+				-- Show the ghost text when the menu is open
+				show_with_menu = false,
+				-- Show the ghost text when the menu is closed
+				show_without_menu = false,
+			},
+			documentation = {
+				-- Controls whether the documentation window will automatically show when selecting a completion item
+				auto_show = true,
+				-- Delay before showing the documentation window
+				auto_show_delay_ms = 50,
+				-- Delay before updating the documentation window when selecting a new item,
+				-- while an existing item is still visible
+				update_delay_ms = 50,
+				-- Whether to use treesitter highlighting, disable if you run into performance issues
+				treesitter_highlighting = true,
+				-- Draws the item in the documentation window, by default using an internal treessitter based implementation
+				draw = function(opts)
+					opts.default_implementation()
+				end,
+				window = {
+					min_width = 10,
+					max_width = 80,
+					max_height = 20,
+					border = "padded",
+					winblend = 0,
+					winhighlight = "Normal:BlinkCmpDoc,FloatBorder:BlinkCmpDocBorder,EndOfBuffer:BlinkCmpDoc",
+					-- Note that the gutter will be disabled when border ~= 'none'
+					scrollbar = true,
+					-- Which directions to show the documentation window,
+					-- for each of the possible menu window directions,
+					-- falling back to the next direction when there's not enough space
+					direction_priority = {
+						menu_north = { "e", "w", "n", "s" },
+						menu_south = { "e", "w", "s", "n" },
+					},
+				},
+			},
+		},
+		-- Default list of enabled providers defined so that you can extend it
+		-- elsewhere in your config, without redefining it, due to `opts_extend`
+		sources = {
+			default = { "lsp", "path", "snippets", "buffer", "copilot" },
+			providers = {
+				copilot = {
+					name = "copilot",
+					module = "blink.compat.source",
+					-- score_offset raises/lower priority; tweak as needed
+					score_offset = 10,
+					opts = {}, -- forwarded to the copilot-cmp source if you need to
+				},
+			},
+		},
+
+		-- Blink.cmp uses a Rust fuzzy matcher by default for typo resistance and significantly better performance
+		-- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
+		-- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
+		--
+		-- See the fuzzy documentation for more information
+		fuzzy = { implementation = "rust" },
+	},
+	opts_extend = { "sources.default" },
+}
