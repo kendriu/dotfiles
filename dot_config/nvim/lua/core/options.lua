25fd1a6f605ab65324a938004fec1385dd2d00c6
Andrzej Skupień <kendriu@gmail.com>
2026-01-23 12:21:09 +0100

    Add .config/nvim/init.lua Add .config/nvim/lazy-lock.json Add .config/nvim/lua/core/keymaps.lua Add .config/nvim/lua/core/neovide.lua Add .config/nvim/lua/core/options.lua Add .config/nvim/lua/plugins/alpha.lua Add .config/nvim/lua/plugins/autoformatting.lua Add .config/nvim/lua/plugins/blink.lua Add .config/nvim/lua/plugins/bufferline.lua Add .config/nvim/lua/plugins/chezmoi.lua Add .config/nvim/lua/plugins/codecompanion.lua Add .config/nvim/lua/plugins/colortheme.lua Add .config/nvim/lua/plugins/conform.lua Add .config/nvim/lua/plugins/copilot.lua Add .config/nvim/lua/plugins/delimiters.lua Add .config/nvim/lua/plugins/diagnostics.lua Add .config/nvim/lua/plugins/docs.lua Add .config/nvim/lua/plugins/.luarc.json Add .config/nvim/lua/plugins/flash.lua Add .config/nvim/lua/plugins/gitsigns.lua Add .config/nvim/lua/plugins/http.lua Add .config/nvim/lua/plugins/ibl.lua Add .config/nvim/lua/plugins/indent-object.lua Add .config/nvim/lua/plugins/lsp.lua Add .config/nvim/lua/plugins/lualine.lua Add .config/nvim/lua/plugins/misc.lua Add .config/nvim/lua/plugins/render-markdown.lua Add .config/nvim/lua/plugins/sidekick.lua Add .config/nvim/lua/plugins/snacks.lua Add .config/nvim/lua/plugins/spelunk.lua Add .config/nvim/lua/plugins/todo.lua Add .config/nvim/lua/plugins/treesitter.lua Add .config/nvim/lua/plugins/trouble.lua

---
+++ dot_config/nvim/lua/core/options.lua
@@ -0,0 +1,34 @@
+vim.opt.number = true -- Make line numbers default
+vim.opt.relativenumber = true -- Set relative numbered lines
+vim.opt.signcolumn = "yes" -- Always show signcolumn
+-- vim.opt.clipboard = "unnamedplus" -- Sync clipboard between OS and Neovim
+vim.opt.wrap = false -- Display lines as one long line
+vim.opt.linebreak = true -- Companion to wrap don't split words
+vim.opt.mouse = "a" -- Enable mouse mode
+vim.opt.autoindent = true -- Copy indent from current line when starting new line
+vim.opt.ignorecase = true -- Case-insensitive searching UNLESS \C or capital in search
+vim.opt.smartcase = true -- Smart case
+vim.opt.scrolloff = 10 -- minimal number of screen lines to keep above and below the cursor
+vim.opt.sidescrolloff = 8 -- minimal number of screen columns either side of cursor if wrap is `false`
+vim.opt.tabstop = 4 -- insert n spaces for a tab
+vim.opt.softtabstop = 4 -- Number of spaces that a tab counts for while performing editing operations
+vim.opt.shiftwidth = 4 -- the number of spaces inserted for each indentation
+vim.opt.expandtab = true -- convert tabs to spaces
+vim.opt.cursorline = true -- highlight the current line
+vim.opt.showmode = false -- Don't show the mode, since it's already in the status line
+vim.opt.undofile = true -- Save undo history
+vim.opt.splitbelow = true -- force all horizontal splits to go below current window
+vim.opt.splitright = true -- force all vertical splits to go to the right of current window
+-- vim.opt.textwidth = 140
+
+vim.g.python3_host_prog = "/Users/andrzej.skupien/neovim_python/.venv/bin/python"
+
+local group = vim.api.nvim_create_augroup("", { clear = true })
+
+vim.api.nvim_create_autocmd("filetype", {
+	pattern = "conf",
+	callback = function()
+		vim.bo.filetype = "yaml"
+	end,
+	group = group,
+})
