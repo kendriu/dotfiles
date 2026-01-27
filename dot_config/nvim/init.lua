9355b5d23d03dbf1e1902a2134a5882578a0377d
Andrzej Skupien <kendriu@gmail.com>
2026-01-27 19:03:37 +0100

    Update .config/nvim/init.lua Add .config/nvim/lua/core/autocmd.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,5 +1,6 @@
 require("core.options")
 require("core.keymaps")
+require("core.autocmd")
 
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
