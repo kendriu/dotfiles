3987adaeb920f1b7edb27f96e9c52b6617521fcd
Andrzej Skupien <kendriu@gmail.com>
2026-02-07 13:09:01 +0100

    Remove .config/fish/custom/21-grc-config.fish Update 10-install-packages.fish

--- dot_config/private_fish/custom/21-grc-config.fish
+++
@@ -1,8 +0,0 @@
-# grc plugin configuration
-# Add executables to ignore here to prevent wrapper conflicts
-
-# Ensure grc is in PATH before plugin checks for it
-fish_add_path -g /opt/homebrew/bin
-
-# Prevent grc from wrapping itself
-set -g grc_plugin_ignore_execs grc
