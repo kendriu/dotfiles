c25d651c6948541a5f3d6053cf104c9f75ad5b48
Andrzej Skupien <kendriu@gmail.com>
2026-02-09 10:11:14 +0100

    Update .config/fish/custom/99-init.fish

--- dot_config/private_fish/custom/99-init.fish
+++ dot_config/private_fish/custom/99-init.fish
@@ -3,6 +3,24 @@
 
 if type -q mise
     mise activate fish | source
+    if ! type -p usage &>/dev/null
+        echo >&2
+        echo "Error: usage CLI not found. This is required for completions to work in mise." >&2
+        echo "See https://usage.jdx.dev for more information." >&2
+        return 1
+    end
+    set -l tmpdir (if set -q TMPDIR; echo $TMPDIR; else; echo /tmp; end)
+    set -l spec_file "$tmpdir/usage__usage_spec_mise_2026_2_5.spec"
+    if not test -f "$spec_file"
+        mise usage | string collect >"$spec_file"
+    end
+
+    set -l tokens
+    if commandline -x >/dev/null 2>&1
+        complete -xc mise -a "(command usage complete-word --shell fish -f \"$spec_file\" -- (commandline -xpc) (commandline -t))"
+    else
+        complete -xc mise -a "(command usage complete-word --shell fish -f \"$spec_file\" -- (commandline -opc) (commandline -t))"
+    end
 end
 
 # zoxide - smart cd with error handling
